93668015bd77a64a8c599530d911f33db2f1a7d5
lucianes1 <lucian_es@hotmail.com>
2021-09-14 15:30:26 -0300

    add snackbar info/ error mixin

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,19 +1,24 @@
 import 'package:filmes/application/ui/loader/loader_mixin.dart';
+import 'package:filmes/application/ui/messages/messages_mixin.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController with LoaderMixin {
+class LoginController extends GetxController with LoaderMixin, MessagesMixin {
   final loading = false.obs;
+  final message = Rxn<MessageModel>();
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     loaderListener(loading);
+    messageListener(message);
   }
 
   Future<void> login() async {
-    loading.value = true;
-    await Future.delayed(Duration(seconds: 2));
-    loading.value = false;
+    loading(true);
+    await 2.seconds.delay();
+    loading(false);
+    message(
+        MessageModel.error(title: 'Titulo Erro', message: 'Mensagem de erro'));
   }
 }
